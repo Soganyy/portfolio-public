048f783afa50b51a9e464e221290a73dda2f4f43
Soganyy <matinmammadli0420@gmail.com>
2025-04-16 01:07:51 +0400

    ui issue fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,7 @@ import Contact from "./Contact";
 export default function Home() {
   return (
     <div>
-      <div className="absolute top-[-200px] left-1/2 -translate-x-1/2 w-[700px] h-[300px] bg-blue-500 opacity-15 rounded-full blur-3xl pointer-events-none z-0" />
+      <div className="fixed top-[-200px] left-1/2 -translate-x-1/2 w-[700px] h-[300px] bg-blue-500 opacity-15 rounded-full blur-3xl pointer-events-none z-0" />
 
       <div className="relative z-10">
         <Hero />
