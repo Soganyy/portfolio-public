9d222a5eb5dfc33849b79db5d6e659b6cad473b6
Soganyy <matinmammadli0420@gmail.com>
2025-04-17 16:11:30 +0400

    ui changes

--- src/pages/Home/Contact.tsx
+++ src/pages/Home/Contact.tsx
@@ -6,40 +6,38 @@ const Contact = () => {
       id="contact"
       className="container mx-auto py-10"
     >
-      <div className="container mx-auto">
-        <div className="flex justify-center space-x-6">
-          <a
-            target="_blank"
-            href="https://www.instagram.com/soganyy/"
-            className="p-3"
-          >
-            <Instagram className="h-6 w-6" />
-            <span className="sr-only">Instagram</span>
-          </a>
-          <a
-            target="_blank"
-            href="https://github.com/Soganyy"
-            className="p-3"
-          >
-            <Github className="h-6 w-6" />
-            <span className="sr-only">GitHub</span>
-          </a>
-          <a
-            href="mailto:matinmammadli0420@gmail.com"
-            className="p-3"
-          >
-            <Mail className="h-6 w-6" />
-            <span className="sr-only">Email</span>
-          </a>
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/matin-mammadli-b05167225/"
-            className="p-3"
-          >
-            <Linkedin className="h-6 w-6" />
-            <span className="sr-only">LinkedIn</span>
-          </a>
-        </div>
+      <div className="flex justify-center space-x-6">
+        <a
+          target="_blank"
+          href="https://www.instagram.com/soganyy/"
+          className="p-3"
+        >
+          <Instagram className="h-6 w-6" />
+          <span className="sr-only">Instagram</span>
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/Soganyy"
+          className="p-3"
+        >
+          <Github className="h-6 w-6" />
+          <span className="sr-only">GitHub</span>
+        </a>
+        <a
+          href="mailto:matinmammadli0420@gmail.com"
+          className="p-3"
+        >
+          <Mail className="h-6 w-6" />
+          <span className="sr-only">Email</span>
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/matin-mammadli-b05167225/"
+          className="p-3"
+        >
+          <Linkedin className="h-6 w-6" />
+          <span className="sr-only">LinkedIn</span>
+        </a>
       </div>
     </section>
   );
