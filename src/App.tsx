a2c814a8f094d7a4df4965743ed59d9d01e98c37
Matin <matinmammadli0420@gmail.com>
2025-06-10 00:01:05 +0400

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { useEffect } from "react";
 import AppRoutes from "./routes";
 import { ThemeProvider } from "./stores/theme-provider";
-import { toast, Toaster } from "sonner";
+import { Toaster } from "sonner";
 
 function App() {
-  useEffect(() => {
-    console.log(1);
-
-    toast("Hey! Thanks for dropping by. Just a quick note — this isn’t a portfolio site.");
-  }, []);
-
   return (
     <ThemeProvider>
       <AppRoutes />
